6f616c666c89eab1acbcdf5e19a5c0547f3e3da6
aleksandardenic <denicaleksandar90@gmail.com>
2021-06-18 15:39:32 +0200

    BasicPage

--- src/pages/BasicPage.java
+++ src/pages/BasicPage.java
@@ -1,5 +1,13 @@
 package pages;
 
-public class BasicPage {
+import org.openqa.selenium.WebDriver;
+
+public abstract class BasicPage {
+	
+	protected WebDriver driver;
+	
+	public BasicPage (WebDriver driver) {
+		this.driver = driver;
+	}
 
 }
