52d0c83b96239bb49e7581f9a6492de5990f0704
aleksandardenic <denicaleksandar90@gmail.com>
2021-06-21 23:50:49 +0200

    ProfileTest

--- src/tests/ProfileTest.java
+++ src/tests/ProfileTest.java
@@ -1,5 +1,18 @@
 package tests;
 
-public class ProfileTest {
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class ProfileTest extends BasicTest {
+	
+	@Test
+	public void editProfile () {
+		driver.get(baseURL + "/guest-user/login-form");
+		
+		locationPopupPage.closeLocation();
+		loginPage.login(username, password);
+		
+		Assert.assertTrue(this.notificationSistemPage.textMessage(), "Login Successfull");
+	}
 
 }
