a891df27af104aa5ca5f7a46ee11bad619e68ef1
aleksandardenic <denicaleksandar90@gmail.com>
2021-06-21 23:47:55 +0200

    NotificationSistemPage

--- src/pages/NotificationSistemPage.java
+++ src/pages/NotificationSistemPage.java
@@ -1,5 +1,37 @@
 package pages;
 
-public class NotificationSistemPage {
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class NotificationSistemPage extends BasicPage {
+
+	public NotificationSistemPage(WebDriver driver, JavascriptExecutor js, WebDriverWait waiter) {
+		super(driver, js, waiter);
+		
+	}
+	
+	public WebElement getMessage () {
+		return driver.findElement(By.xpath("//*[contains(@class, 'alert--success') or contains(@class, 'alert--danger')][contains(@style,'display: block')]"));
+	    }
+	
+	public String messageText () {
+		return this.driver.findElement(By.xpath("//div[@class='div_msg']/ul/li")).getText();
+	}
+	
+	public void waitMessage () {
+		WebDriverWait wait = new WebDriverWait(driver, 5);
+		wait.until(ExpectedConditions.attributeToBe(By.xpath("//*[contains(@class, 'system_message')]"), "style", "display: none;"));
+	}
+	
+	public boolean textMessage () {
+		List<WebElement>text = driver.findElements(By.xpath("//*[contains(@class, 'alert--success') or contains(@class, 'alert--danger')][contains(@style,'display: block')]"));
+		return text.size() > 0;
+	}
 
 }
