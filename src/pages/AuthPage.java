313ef5217ee9eaa982020c2bedd649e1d5cf77e4
aleksandardenic <denicaleksandar90@gmail.com>
2021-06-22 16:19:48 +0200

    AuthPage

--- src/pages/AuthPage.java
+++ src/pages/AuthPage.java
@@ -1,5 +1,32 @@
 package pages;
 
-public class AuthPage {
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class AuthPage extends BasicPage {
+
+	public AuthPage(WebDriver driver, JavascriptExecutor js, WebDriverWait waiter) {
+		super(driver, js, waiter);
+		
+	}
+
+	public WebElement getClick () {
+		return driver.findElement(By.xpath("//*[@class=\"right-head\"]/div[2]"));
+	    }
+	
+	public WebElement getClickDown () {
+		return driver.findElement(By.xpath("//*[@class=\"right-head\"]/div[2]/ul/li/div/ul/li[2]"));
+	    }
+	
+	public void logingOut () {
+		this.getClick().click();
+		this.getClickDown().click();
+	}
+	
+
+	
 
 }
