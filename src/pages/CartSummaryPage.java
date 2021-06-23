2495ffb538d9a328c895cd9746808a245e9fa349
aleksandardenic <denicaleksandar90@gmail.com>
2021-06-24 00:56:00 +0200

    CartSummaryPage

--- src/pages/CartSummaryPage.java
+++ src/pages/CartSummaryPage.java
@@ -1,5 +1,24 @@
 package pages;
 
-public class CartSummaryPage {
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class CartSummaryPage extends BasicPage{
+
+	public CartSummaryPage(WebDriver driver, JavascriptExecutor js, WebDriverWait waiter) {
+		super(driver, js, waiter);
+		
+	}
+	
+	public WebElement getClearAll () {
+		return driver.findElement(By.xpath("//*[@class=\"cart-head\"]/a[2]"));
+	}
+	
+	public void clearAll () {
+		this.getClearAll().click();
+	}
 
 }
